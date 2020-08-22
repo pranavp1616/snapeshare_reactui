0ea2cf8533c8cb6f9695233f3056185e9942c636
pranavp1616 <pranavp1616@gmail.com>
2020-08-23 03:35:39 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -6,11 +6,29 @@ import RegisterPage from './pages/RegisterPage';
 import Home from './pages/HomePage';
 import MyProfile from './pages/MyProfile';
 
+import {Route,BrowserRouter as Router} from 'react-router-dom';
+
+/*
 var myjsx = <div>
                 <LoginPage />
                 <RegisterPage />
                 <Home />
                 <MyProfile />
-            </div>  
+            </div>
+*/
+
+var myjsx = <Router>
+                <div> 
+                    <a href='/login'>Login</a>
+                    <a href='/register'>Register</a>
+                    <a href='/home'>Home</a>
+                    <a href='/myprofile'>Myprofile</a>
+                </div>
+                <Route path='/' exact component={Home} />
+                <Route path='/home' component={Home} />
+                <Route path='/login' component={LoginPage} />
+                <Route path='/register' component={RegisterPage} />
+                <Route path='/myprofile' component={MyProfile} />
+            </Router>
     
 ReactDOM.render(myjsx,document.getElementById('root'));
