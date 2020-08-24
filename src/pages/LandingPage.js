b201eee5e562340c524d3db6016fe10ccc9c9f0f
pranavp1616 <pranavp1616@gmail.com>
2020-08-24 16:13:25 +0530

    loginpage all working css fixed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Login from '../components/Login';
-import Register from '../components/Register';
 
 class LandingPage extends React.Component{
     render(){
         return  <div>
-                    <Login />
-                    <a href='/register'>Register</a>
+                    <div className='row'>
+                        <div className='container center'>
+                            Snapeshare and logo
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <Login />
+                    </div>
+                    <div className='row'>
+                        <div className='container center'>
+                            <a href='/register'>Create a new account?</a>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='container center'>
+                            Tech stack
+                            <br/>
+                            <a href='https://github.com/pranavp1616/snapeshare_reactui'>React JS code</a>
+                            <br/>
+                            <a href='https://github.com/pranavp1616/snapshare_api/'>Python Django code</a>
+                            <br/>
+                            Deployed on Amazon AWS and heroku 
+                            <br/>
+                            <h1>Insert wallpaper in this landing page</h1>
+                        </div>
+                    </div>
                 </div>
     }
 }
