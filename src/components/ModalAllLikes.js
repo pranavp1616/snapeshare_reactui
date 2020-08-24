85ddb768a57868130d5281128b8dee0b68409fae
pranavp1616 <pranavp1616@gmail.com>
2020-08-25 03:46:00 +0530

    like modal call api working

--- src/components/ModalAllLikes.js
+++ src/components/ModalAllLikes.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Modalstyle} from './Global'
+import {API} from './Global';
 
 class ModalAllLikes extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            all_likes : []
+        }
+    }
+    async getAllLikes(){
+        var url = API + 'getlikes/'+this.props.post_id;
+        var headers = new Headers();
+        headers.append('Authorization','Token '+localStorage.auth_token);
+        var request = new Request(url,  {  method:'GET',headers }   );
+        var resp = await fetch(request);
+        var data = await resp.json();
+        console.log(data);
     }
     render() {
+        if(this.props.isOpen == true)
+            this.getAllLikes();
         return (
              <div>
                  {  this.props.isOpen == true
@@ -14,7 +29,7 @@ class ModalAllLikes extends React.Component{
                         <div className='card' style={{marginTop:'100px', marginLeft:'10%', marginRight:'10%'}}>
                             <div className='card-content'>
                                 <button onClick={this.props.onClose} className='btn'>x</button>
-                                All likes {this.props.post_id}
+                                All likes of {this.props.post_id}
                             </div>
                         </div>
                     </div>
