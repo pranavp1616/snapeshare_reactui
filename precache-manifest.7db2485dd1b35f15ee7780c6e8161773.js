e1892e962d8a4c6d56816cd3dea0ed1b75da96da
pranavp1616 <pranavp1616@gmail.com>
2020-08-28 03:20:41 +0530

    Updates

---
+++ precache-manifest.7db2485dd1b35f15ee7780c6e8161773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b127bd6e4a826ebf532522e19ed06801",
+    "url": "/snapshare_reactui/index.html"
+  },
+  {
+    "revision": "25f4d75379372f224635",
+    "url": "/snapshare_reactui/static/js/2.9e66d63a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snapshare_reactui/static/js/2.9e66d63a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f3e3adf1c5401977e0",
+    "url": "/snapshare_reactui/static/js/main.d3d3d601.chunk.js"
+  },
+  {
+    "revision": "dd521d13d0da84e4b02f",
+    "url": "/snapshare_reactui/static/js/runtime-main.72a355ea.js"
+  }
+]);
