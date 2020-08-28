2e3861ed0efb4575ec7cb73fd01de0213e991fb6
pranavp1616 <pranavp1616@gmail.com>
2020-08-29 04:05:11 +0530

    upload image removed document object vanila JS code

--- src/components/UploadPost.js
+++ src/components/UploadPost.js
@@ -4,23 +4,28 @@ import {API} from './Global';
 class UploadPost extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            image_src : '',
+            hashtags : ''
+        }
         this.uploadPostLogic = this.uploadPostLogic.bind(this);
+        this.handleInputFileChange = this.handleInputFileChange.bind(this);
     }
 
     async uploadPostLogic(){
-        // change this direct DOM access through document  to state later
-        var image = document.getElementById('upload_input_file').files[0];
-        var hashtags = document.getElementById('upload_input_hashtag').value;
-
         var url = API +'photopost/create/';
         var headers = new Headers();
         headers.append('Authorization','Token '+localStorage.auth_token);
         var formData = new FormData();  
-        formData.append('image',image);  
-        formData.append('hashtags',hashtags);
+        formData.append('image',this.state.image_src);  
+        formData.append('hashtags',this.state.hashtags);
         var request = new Request(url, {method:'POST', headers, body:formData}  );
         await fetch(request);
         window.location.reload();
+    }
+
+    handleInputFileChange(e){
+        this.setState({image_src:e.target.files[0]})
     }
 
     render(){
@@ -31,13 +36,13 @@ class UploadPost extends React.Component{
                             <div className="file-field input-field">
                                 <div className="btn indigo">
                                     <span>browse</span>
-                                    <input type="file" id='upload_input_file' accept="image/*"/>
+                                    <input type="file" onChange={this.handleInputFileChange} accept="image/*"/>
                                 </div>
                                 <div class = "file-path-wrapper">
                                     <input class = "file-path validate" type = "text" placeholder = "Selected file" />
                                 </div>
                             </div>
-                            <input type="text" id='upload_input_hashtag' placeholder='hashtags?'/> 
+                            <input type="text" onChange={ e => this.setState({ hashtags : e.target.value})} placeholder='hashtags?'/> 
                             <button onClick={this.uploadPostLogic}
                                             className='btn indigo'>
                                             Post
