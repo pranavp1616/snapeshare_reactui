1a8ea0e271d23520a3526911fdeff5d2ce4c298d
pranavp1616 <pranavp1616@gmail.com>
2020-09-13 07:08:14 +0530

    github link fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,8 +8,8 @@ class LandingPage extends React.Component{
                     <Logo />
                     <Login />
                     <h5><a href='/register'>Create a new account?</a></h5>
-                    <b>REST API</b> <a href='https://github.com/pranavp1616/snapshare_api/'>Django (python) github</a>
-                    <b>UI</b> <a href='https://github.com/pranavp1616/snapeshare_reactui'>React (JS) github</a>
+                    <b>REST API</b> <a href='https://github.com/pranavp1616/snapshare_nodejs_api'>Node JS (Express) github</a>
+                    <b>UI</b> <a href='https://github.com/pranavp1616/snapeshare_reactui'>React JS github</a>
                 </div>
     }
 }
