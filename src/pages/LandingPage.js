78a99f3e4a29e2d27d76f1a3a7026026a4a1e830
pranavp1616 <pranavp1616@gmail.com>
2020-09-14 19:30:28 +0530

    github links

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,9 +11,10 @@ class LandingPage extends React.Component{
                             <div style={{margin:'20px'}}>
                                 <h5><a href='/register'>Create a new account?</a></h5>
                             </div>
-                            <div style={{margin:'20px'}}>
-                                <h6><b>REST API</b> <a href='https://github.com/pranavp1616/snapshare_nodejs_api'>Node JS (Express) github</a></h6>
-                                <h6><b>UI</b> <a href='https://github.com/pranavp1616/snapeshare_reactui'>React JS github</a></h6>
+                            <div style={{margin:'30px'}}>
+                                <h5>Github</h5>
+                                <h6>API <a href='https://github.com/pranavp1616/snapshare_nodejs_api'>Node JS Express</a></h6>
+                                <h6>UI <a href='https://github.com/pranavp1616/snapeshare_reactui'>React JS</a></h6>
                             </div>
                     </div>
                 </div>
