8ee1404037ec7d032c97af04d7b692426d77009e
pranavp1616 <pranavp1616@gmail.com>
2020-09-14 19:32:17 +0530

    Css changes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class Logo extends React.Component{
     render() {
-            return  <div style={{margin:'70px'}}>
-                        <h2>Snapshare</h2>
+            return  <div style={{margin:'50px'}}>
+                        <h1>Snapshare</h1>
                     </div>
     }
 }
