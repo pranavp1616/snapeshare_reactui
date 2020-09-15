349874c1ffa3bf9d3e4ca1603d09e0bf855c1f32
pranavp1616 <pranavp1616@gmail.com>
2020-09-15 09:07:57 +0530

    localhostip config

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,5 +1,7 @@
-const API = 'https://snapshare-nodejs.herokuapp.com/api/';
-//const API = 'http://127.0.0.1:8000/api/';
+// PRODUCTION IP (AWS EC2)
+const API = 'http://18.220.224.148:3000/api/'; 
+// Localhost IP
+//const API = 'http://locahost:8000/api/';
 
 // add USER_AUTH_TOKEN and LOGGED_IN_USERNAME here as global variables later
 
