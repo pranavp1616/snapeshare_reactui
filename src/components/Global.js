2009d0d1dd01942f7c1b68ec248516bf1c1ef220
pranavp1616 <pranavp1616@gmail.com>
2020-09-16 23:12:15 +0530

    set PROD API

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,7 +1,3 @@
-/* PRODUCTION IP (AWS-EC2 HTTPS NGINIX)  */
-const API = 'https://snapshare.live/api/'; 
-
-/* Localhost IP */
-// const API = 'http://localhost:8000/api/'; 
+const API = 'http://localhost:8000/api/'; 
 
 export {API}
