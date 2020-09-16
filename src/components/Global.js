cdd6764f1961f324d26956805dceaaf43b6a84f5
pranavp1616 <pranavp1616@gmail.com>
2020-09-16 08:41:41 +0530

    PROD and DEV IP config

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,8 +1,7 @@
-// PRODUCTION IP (AWS EC2 instance with domain name thoughtmesh.xyz - HTTPS NGINIX)
+/* PRODUCTION IP (AWS-EC2 HTTPS NGINIX)  */
 const API = 'https://thoughtmesh.xyz/api/'; 
 
-// Localhost IP
-//const API = 'http://locahost:8000/api/';
+/* Localhost IP */
+// const API = 'http://localhost:8000/api/'; 
 
-// add USER_AUTH_TOKEN and LOGGED_IN_USERNAME here as global variables later
 export {API}
